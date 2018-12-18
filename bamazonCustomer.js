94ffaf541fa7e745bbbe674ff88ccd641364115f
Bijan Parvin <bparvin12@gmail.com>
2018-12-18 14:54:05 -0800

    finished with basics

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -22,40 +22,53 @@ connection.query('SELECT * FROM products', function (error, results, fields) {
             "\nPrice: $" + results[i].price +
             "\n-----------------------------------");
     }
-    //this function brings up inqurier 
-    questions = function () {
-
-        inquirer.prompt([
-            {
-                type: "input",
-                message: "Which item would you like to buy today (Use the Product ID Number)?",
-                name: "productid"
-            },
-            {
-                type: "input",
-                message: "How many units would you like to buy?",
-                name: "quantity"
-            },
-        ]).then(function (user) {
-            var idNum = user.productid
-            console.log("----------------------------------------");
-            console.log("Item in cart: " + results[idNum - 1].product_name);
-            console.log("Quantity desired: " + user.quantity);
-            console.log("----------------------------------------");
-            if (user.quantity > results[idNum - 1].stock_quantity) {
-                console.log("I am sorry, but we do not have enough of this item in stock.");
-
-            } else {
-                console.log("You are buying (" + user.quantity + ") " + results[idNum - 1].product_name
-                    + ". \nYour total amount due is: $" + results[idNum - 1].price * user.quantity +
-                    "\n------------------------------");
-                var newQuantity = parseInt(results[idNum - 1].stock_quantity) - parseInt(user.quantity);
-                substractFromDatabase(newQuantity, idNum);
-            }
-        });
-    }
     questions();
 });
+//this function brings up inqurier 
+questions = function () {
+
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "Which item would you like to buy today (Use the Product ID Number)?",
+            name: "productid"
+        },
+        {
+            type: "input",
+            message: "How many units would you like to buy?",
+            name: "quantity"
+        },
+    ]).then(function (user) {
+        connection.query('SELECT * FROM products WHERE ?',
+            [
+                {
+                    item_id: user.productid
+                },
+            ],
+            function (error, results) {
+                console.log("----------------------------------------");
+                console.log("Item in cart: " + results[0].product_name);
+                console.log("Quantity desired: " + user.quantity);
+                console.log("----------------------------------------");
+                var idNum = results[0].item_id;
+
+                if (user.quantity > results[0].stock_quantity) {
+                    console.log("I am sorry, but we do not have enough of this item in stock.");
+
+                } else {
+                    var newQuantity = parseInt(results[0].stock_quantity) - parseInt(user.quantity);
+                    console.log("You are buying (" + user.quantity + ") " + results[0].product_name
+                        + ". \nYour total amount due is: $" + results[0].price * user.quantity +
+                        "\nThere are only " + newQuantity + " of this item left!" +
+                        "\n------------------------------");
+                    
+                    substractFromDatabase(newQuantity, idNum);
+                    
+                }
+            });
+    });
+}
+
 
 function substractFromDatabase(amazonQuantity, amazonID) {
     var query = connection.query('UPDATE products SET ? WHERE ?',
@@ -67,13 +80,13 @@ function substractFromDatabase(amazonQuantity, amazonID) {
                 item_id: amazonID
             }
         ],
-        function (error, results) {
-            console.log(error);
-            console.log(query.sql);
+        function (results) {
             connection.end();
         }
     );
 };
+
+
 
 
 
